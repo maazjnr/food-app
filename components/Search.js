aa9353c8eb5c741f123c49f6a6dd304940396993
Maaz Junior <goalmaaz@gmail.com>
2022-10-05 10:31:47 +0100

    working on search function

--- components/Search.js
+++ components/Search.js
@@ -1,47 +1,122 @@
-import { View, Text, TextInput, } from 'react-native'
-import React, { useState } from 'react'
-import { Ionicons } from "@expo/vector-icons";
-import { EvilIcons } from "@expo/vector-icons";
+import React, { useState, useEffect } from 'react';
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  TextInput,
+} from 'react-native';
 
 const Search = () => {
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+  const myApi = "dc08124ff78a4ea9855372247525457d";
 
- const [input, setInput] = useState("")
+  useEffect(() => {
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${myApi}&number=10`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson.results);
+        setMasterDataSource(responseJson.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource and update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        // Applying filter for the inserted text in search bar
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert('Id : ' + item.id + ' Title : ' + item.title);
+  };
 
   return (
-    <View
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      position: "relative",
-    }}
-  >
-    <TextInput
-      placeholder="Search by Customer"
-      value={input}
-      onChangeText={setInput}
-      style={{
-        backgroundColor: "#fff",
-        padding: 15,
-        margin: 5,
-        marginBottom: 10,
-        borderRadius: 10,
-        borderWidth: 1,
-      }}
-    />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => searchFilterFunction(text)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Search Here"
+        />
+        <FlatList
+          data={filteredDataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-    <EvilIcons
-      style={{
-        position: "absolute",
-        padding: 5,
-        right: 25,
-      }}
-      name="search"
-      size={24}
-      color="black"
-    />
-  </View>
-  )
-}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+  itemStyle: {
+    padding: 10,
+  },
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
+    marginBottom: 200
+  },
+});
 
-export default Search
+export default Search;
--- screens/Searched.js
+++ screens/Searched.js
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { Ionicons } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
 
-const Searched = () => {
+const Search = () => {
+
+ const [input, setInput] = useState("")
+
+
   return (
-    <View>
-      <Text>Searched</Text>
-    </View>
+    <View
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      position: "relative",
+    }}
+  >
+    <TextInput
+      placeholder="Search by Customer"
+      value={input}
+      onChangeText={setInput}
+      style={{
+        backgroundColor: "#fff",
+        padding: 15,
+        margin: 5,
+        marginBottom: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+      }}
+    />
+
+    <EvilIcons
+      style={{
+        position: "absolute",
+        padding: 5,
+        right: 25,
+      }}
+      name="search"
+      size={24}
+      color="black"
+    />
+  </View>
   )
 }
 
-export default Searched
+export default Search
