5384ba29301c4ff47a397c4ed1ae8ce5ea946c07
Maaz Junior <goalmaaz@gmail.com>
2022-10-09 22:23:27 +0100

    working

--- components/Search.js
+++ components/Search.js
@@ -1,36 +1,69 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Searched from '../screens/Searched'
 import axios from 'axios'
 
 const Search = () => {
 
-  const [searchText, setSearchText] = useState('')
-  const [data, setData] = useState('')
+  const myApi = "dc08124ff78a4ea9855372247525457d";
 
-  const options = {
-    method: 'GET',
-    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random',
-    params: {tags: 'vegetarian,dessert', number: '1'},
-    headers: {
-      'X-RapidAPI-Key': 'dc811aafffmsh7cf2c61db109937p1ffcfajsne527db6bea89',
-      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-    }
-  };
-  
-  axios.request(options).then(function (response) {
-    setData(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
+  const [searchText, setSearchText] = useState('')
+  const [myData, setMyData] = useState([])
+
+ const searchDataReq = () => {
+  fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${myApi}&number=3`
+  )
+    .then((response) => response.json())
+    .then((responseJson) => {
+      setMyData(responseJson.results);
+     
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+ }
+
+ useEffect(() => {
+  searchDataReq()
+}, []);
+
+
+// const searchDataReq = () => {
+
+//   const options = {
+//     method: 'GET',
+//     url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random',
+//     params: {tags: 'vegetarian,dessert', q: searchText,  number: '1'},
+//     headers: {
+//       'X-RapidAPI-Key': 'dc811aafffmsh7cf2c61db109937p1ffcfajsne527db6bea89',
+//       'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+//     }
+//   };
+
+//   axios.get(options).then(function (response) {
+//     setMyData(response.data);
+//   }).catch(function (error) {
+//     console.error(error);
+//   });
+
+// }
+
 
   return (
     <View style={styles.container}>
      <Searched 
      searchText={searchText} 
      setSearchText={setSearchText}
+     onSubmit={searchDataReq}
      />
-     <Text>{searchText}</Text>
+     <FlatList data={myData}
+     renderItem={({item}) => <Searched
+     urlToImage={item.image}
+     title={item.title}
+     /> } keyExtractor={(item) => item.title}
+      />
     </View>
   )
 }
--- screens/Searched.js
+++ screens/Searched.js
@@ -1,23 +1,37 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from "react-native";
 
-const Searched = ({searchText, setSearchText}) => {
+const Searched = (props) => {
   return (
-    <View style={styles.container}>
-      <TextInput 
-      placeholder='Search Recipe'
-      style={styles.input}
-      value={searchText}
-      onChangeText={(text) => setSearchText(text)}
+    <ScrollView contentContainerStyle={styles.container}>
+      <TextInput
+        placeholder="Search Recipe"
+        style={styles.input}
+        value={props.searchText}
+        onChangeText={(text) => props.setSearchText(text)}
+        onSubmitEditing={props.onSubmit}
       />
-    </View>
-  )
-}
 
+      <Image
+        source={props.urlToImage}
+        resizeMode="cover"
+        style={{ width: 300, height: 300 }}
+      />
+
+      <Text>{props.title}</Text>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    margin: 10
+    margin: 10,
   },
 
   input: {
@@ -26,10 +40,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     color: "#111",
     borderWidth: 1,
-  }
-})
+  },
+});
 
-
-
-
-export default Searched
+export default Searched;
